05d1af46f45053396de49bcec04157410006f071
u22757733 <116093000+V-Jessie-Z@users.noreply.github.com>
2025-06-23 17:19:55 +0200

    final try

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import path from 'path';
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  output: 'standalone', // <-- Add this to bundle Prisma engine correctly
   webpack(config) {
     config.resolve.alias = {
       ...config.resolve.alias,
